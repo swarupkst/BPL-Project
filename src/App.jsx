fb3a269c162c0ca2671735e7e11641fa0c101568
Swarup Biswas <swarupkst@gmail.com>
2026-03-24 00:48:38 +0600

    TailwindCSS and DaisyUI setup complete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-6xl text-center py-8 bg-yellow-900'>Hello , TailwindCSS and DaisyUI setup complete</h1>
+      <button className="btn mx-auto block size-72">
+  <span className="loading loading-spinner"></span>
+  loading
+</button>
+    </>
+  )
+}
+
+export default App
